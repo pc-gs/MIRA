1e25bf10a13ffd859babe7bc5aee9946e495280e
Pedro Santos <pedrocarlos.ti@gmail.com>
2026-04-29 13:42:41 +0100

    Initial Commit

---
+++ Mira/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mira
+//
+//  Created by pcsantos on 29/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mira/MiraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiraApp.swift
+//  Mira
+//
+//  Created by pcsantos on 29/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MiraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
